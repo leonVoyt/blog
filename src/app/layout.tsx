d5d556c66a35b2d579b880d0902cfdccf632e1e0
Leon <6860571@stud.nau.edu.ua>
2023-09-04 18:00:26 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import SignIn from '@/components/SignIn'
 import NavBar from '@/components/NavBar'
-
-const Layout = ({ children }) => {
+interface Props {
+  children: string | JSX.Element | JSX.Element[]
+}
+const Layout: React.FC<Props> = ({ children }) => {
   return (
     <div className="div">
       <NavBar />
